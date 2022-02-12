d6bc77fec5a2ae9cd3f6bf5e1c337f336b5e3415
chaeeunj <official2930@naver.com>
2022-02-13 03:34:37 +0900

    #2
    코드 개선

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
- if (h1.className === "active") {
-   h1.className = "";
- } else {
-   h1.className = "active";
- }
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+   h1.classList.remove(clickedClass);
+  } else {
+   h1.classList.add(clickedClass);
+  }
 } 
-/* 만약 h1태그의 className이 active라면 className을 없애주고 
-   그렇지 않다면 className을 active로 지정하는 함수 */
 
 h1.addEventListener("click", handleTitleClick); // title.onclick = handleTitleClick
 
