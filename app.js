939390a75fdf9566f837aeaa7a84eabc6c839541
chaeeunj <official2930@naver.com>
2022-02-13 02:40:12 +0900

    #1

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-function handleWindowResize() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if(currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-  h1.style.color = newColor
-}
+ if (h1.className === "active") {
+   h1.className = "";
+ } else {
+   h1.className = "active";
+ }
+} 
+/* 만약 h1태그의 className이 active라면 className을 없애주고 
+   그렇지 않다면 className을 active로 지정하는 함수 */
 
 h1.addEventListener("click", handleTitleClick); // title.onclick = handleTitleClick
 
