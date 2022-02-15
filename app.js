753a3e58833c385b3adc71e7e08d3830b713056d
chaeeunj <official2930@naver.com>
2022-02-15 17:47:22 +0900

    Form Submission #2
    no JS, only HTML

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const loginButton = document.querySelector(".login-form button"); // buttoníƒœê·
 
 function loginBtnClick() {
   const username = loginInput.value;
-  if (username === "") {
-    alert("Please write your name");
-  } else if (username.length > 15) {
-    alert("Your name is too long");
-  }
+  console.log(username);
 }
 
 loginButton.addEventListener("click", loginBtnClick)
