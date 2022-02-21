902d61739b5b0eb32afd00156c33db026f14420c
chaeeunj <official2930@naver.com>
2022-02-21 18:34:02 +0900

    4.2 Evnets

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// const loginForm = document.querySelector(".login-form");
+const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input"); // input태그 찾기
-const loginButton = document.querySelector(".login-form button"); // button태그 찾기
 
-function loginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
 }
 
-loginButton.addEventListener("click", loginBtnClick)
+loginForm.addEventListener("submit", onLoginSubmit);
