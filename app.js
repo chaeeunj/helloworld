ff3341e85a454587ff5330bf1077cee84e5b1333
chaeeunj <official2930@naver.com>
2022-02-23 04:03:33 +0900

    Saving Username

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  loginForm.classList.add("HIDDEN_CLASSNAME");
-  greeting.innerText = "Hello " + username;
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove("HIDDEN_CLASSNAME");
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
