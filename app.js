c1202dc35940ef1a97c9662770521fd23e350abc
chaeeunj <official2930@naver.com>
2022-02-23 03:09:51 +0900

    4.4 Getting Username

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(event);
+  const username = loginInput.value;
+  loginForm.classList.add("HIDDEN_CLASSNAME");
+  greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove("HIDDEN_CLASSNAME");
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
-}
-
-link.addEventListener("click", handleLinkClick);
 loginForm.addEventListener("submit", onLoginSubmit);
