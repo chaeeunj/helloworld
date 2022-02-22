1841147cd69187c7d784e7898fb08bbd0d301051
chaeeunj <official2930@naver.com>
2022-02-23 04:55:15 +0900

    Loading Username - use function

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem("USERNAME_KEY", username);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
 const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+function paintGreetings() {
+  greeting.innerText = `Hello ${savedUsername}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  greeting.innerText = `Hello ${savedUsername}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(savedUsername);
 }
