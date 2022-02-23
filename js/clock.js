0d2b198e63a0fe231f93e19b07c1fd0f2eab6c75
chaeeunj <official2930@naver.com>
2022-02-24 04:30:39 +0900

    padStart, padEnd

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const colck = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  colck.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes =  String(date.getMinutes()).padStart(2, "0");
+  const seconds =  String(date.getSeconds()).padStart(2, "0");
+  colck.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
